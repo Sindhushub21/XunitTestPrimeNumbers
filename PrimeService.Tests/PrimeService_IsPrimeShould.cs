6c142175df70600b78254a048cf46c0b1f41c7fa
Sindhushub21 <sindhurachanda@gmail.com>
2021-03-31 12:52:36 -0400

    first commit

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,40 @@
+using Xunit;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    public class PrimeService_IsPrimeShould
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        
+        public void ShouldReturnFalse_WhenPassedNotPrime_IsPrime(int value)
+        {
+            var _primeService = new PrimeService();
+
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        [InlineData(11)]
+        public void ShouldReturnTrue_WhenPassedPrime_IsPrime(int value)
+        {
+            //Given
+            var _primeService = new PrimeService();
+            //When
+            var result = _primeService.IsPrime(value);
+            //Then
+            Assert.True(result, $"{value} should not be prime");
+        }
+    }
+}
